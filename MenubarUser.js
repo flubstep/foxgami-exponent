f40e87c1a5671b7741477d3a259a79f53d55051b
Albert Sheu <albert.sheu@gmail.com>
2015-12-19 14:56:40 -0800

    Add MenubarUser component

---
+++ MenubarUser.js
@@ -0,0 +1,97 @@
+/**
+ * @providesModule MenubarUser
+ */
+
+'use strict';
+
+let React = require('react-native');
+let {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight
+} = React;
+
+let {Colors} = require('./BaseStyles');
+let FoxgamiApi = require('FoxgamiApi');
+
+class MenubarUser extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      currentUser: null,
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    this._fetchData();
+  }
+
+  _fetchData() {
+    FoxgamiApi.getCurrentUser()
+      .then((userInfo) => {
+        this.setState({
+          userId: userInfo.data.id,
+          profileImageUrl: userInfo.data.profile_image_url,
+          shortName: userInfo.data.short_name,
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  _login() {
+
+  }
+
+  _renderWaiting() {
+    return (
+      <Text style={styles.loginText}>Loading...</Text>
+    );
+  }
+
+  _renderLogin() {
+    return (
+      <TouchableHighlight style={styles.loginContainer} onPress={() => this._login()}>
+        <Text style={styles.loginText}>LOG IN</Text>
+      </TouchableHighlight>
+    );
+  }
+
+  _renderUser() {
+    return (
+      <TouchableHighlight style={styles.loginContainer} onPress={() => this._login()}>
+        <Text style={styles.loginText}>{this.state.shortName}</Text>
+      </TouchableHighlight>
+    );
+  }
+
+  render() {
+    if (!this.state.loaded) {
+        return this._renderWaiting();
+    } else if (this.state.userId) {
+        return this._renderUser();
+    } else {
+        return this._renderLogin();
+    }
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  iconNavLogo: {
+    alignItems: 'center',
+    width: 28,
+    height: 28,
+    margin: 10,
+    marginTop: 26,
+  },
+});
+
+module.exports = MenubarUser;
