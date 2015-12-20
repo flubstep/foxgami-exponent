8bf1f44b34d2f785858c0992c3525fb425dfe2cf
Albert Sheu <albert.sheu@gmail.com>
2015-12-20 13:58:17 -0800

    Add input field components to SignupLogin

--- SignupLogin.js
+++ SignupLogin.js
@@ -1,3 +1,7 @@
+/**
+ * @providesModule SignupLogin
+ */
+
 'use strict';
 
 let React = require('react-native');
@@ -7,19 +11,54 @@ let {
   Text,
   View,
   Image,
+  TextInput
 } = React;
 
-let {Colors} = require('./BaseStyles');
+let {Colors} = require('BaseStyles');
 
 class SignupLogin extends React.Component {
 
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      textEmail: '',
+      textPassword: ''
+    }
+  }
+
   render() {
+    /*
+      X out to return to main feed
+      email input field
+      password input field
+      forgot password link
+      sign up link
+    */
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.iconNavLogo}
-          source={require('./images/logo.png')}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(textEmail) => this.setState({textEmail})}
+            value={this.state.textEmail}
+            placeholder={"E-mail"}
+            autoFocus={true}
+            autoCapitalize={false}
+            autoCorrect={false}
+            keyboardType={'email-address'}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputField}
+            onChangeText={(textPassword) => this.setState({textPassword})}
+            value={this.state.textPassword}
+            placeholder={"Password"}
+            autoCapitalize={false}
+            autoCorrect={false}
+            secureTextEntry={true}
+          />
+        </View>
       </View>
     );
   }
@@ -28,6 +67,20 @@ class SignupLogin extends React.Component {
 let styles = StyleSheet.create({
   container: {
     alignItems: 'center',
+    marginTop: 175
+  },
+  inputField: {
+    height: 48,
+    width: 295,
+    fontFamily: 'Gill Sans',
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomColor: '#EEEEEE',
+    borderBottomWidth: 2
   },
   iconNavLogo: {
     alignItems: 'center',
