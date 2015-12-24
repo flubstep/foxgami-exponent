4f7cf4e1a5b1153899d79f30f90cbe18b6d40139
Kathy Zheng <kazzheng@gmail.com>
2015-12-24 14:58:58 -0800

    Flexible height and anchoring to keyboard fixed

--- Auth/SignupLoginScreen.js
+++ Auth/SignupLoginScreen.js
@@ -7,6 +7,8 @@
 let React = require('react-native');
 let {
   AppRegistry,
+  DeviceEventEmitter,
+  Dimensions,
   StyleSheet,
   Text,
   View,
@@ -27,30 +29,45 @@ class SignupLoginScreen extends React.Component {
     this.state = {
       textUsername: '',
       textEmail: '',
-      textPassword: ''
+      textPassword: '',
+      visibleHeight: Dimensions.get('window').height
     }
   }
 
-  _cancel() {
+  componentWillMount () {
+    DeviceEventEmitter.addListener('keyboardWillShow', this.keyboardWillShow.bind(this))
+    DeviceEventEmitter.addListener('keyboardWillHide', this.keyboardWillHide.bind(this))
+  }
+
+  keyboardWillShow (e) {
+    let newSize = Dimensions.get('window').height - e.endCoordinates.height
+    this.setState({visibleHeight: newSize})
+  }
+
+  keyboardWillHide (e) {
+    this.setState({visibleHeight: Dimensions.get('window').height})
+  }
+
+  _back() {
     this.props.navigator.pop();
   }
 
-  _renderCancelButton() {
+  _renderBackButton() {
     return (
-      <IconButton
-        style={styles.cancelButton}
-        onPress={this._cancel.bind(this)}
-        source={require('../images/Back.png')}
-      />
+      <TouchableOpacity onPress={this._onPressButton}>
+        <IconButton
+          style={styles.backButton}
+          onPress={this._back.bind(this)}
+          source={require('../images/Back.png')}
+          />
+      </TouchableOpacity>
     );
   }
 
   _renderLoginButton() {
     if (this.state.textUsername && this.state.textEmail && this.state.textPassword) {
       return (
-        <TouchableHighlight
-          style={styles.loginButton}
-          >
+        <TouchableHighlight style={styles.loginButton}>
           <Text style={styles.loginText}>LOG IN</Text>
         </TouchableHighlight>
       );
@@ -64,9 +81,7 @@ class SignupLoginScreen extends React.Component {
 
   _renderSignupLink() {
     return (
-      <TouchableHighlight
-        style={styles.signupButton}
-        >
+      <TouchableHighlight style={styles.signupButton}>
         <Text style={styles.signupText}>SIGN UP</Text>
       </TouchableHighlight>
     );
@@ -74,12 +89,11 @@ class SignupLoginScreen extends React.Component {
 
   _renderTermsLink() {
     return (
-      <TouchableHighlight
-        style={styles.signupButton}
-        >
+      <TouchableHighlight style={styles.signupButton}>
         <Text style={styles.signupText}>TERMS</Text>
       </TouchableHighlight>
     );
+
   }
 
   render() {
@@ -89,53 +103,59 @@ class SignupLoginScreen extends React.Component {
       email input field
       password input field
       forgot password link
+      terms Link
       sign up link
     */
     return (
-      <View style={styles.container}>
+      <View style={{height: this.state.visibleHeight}}>
         <View style={styles.topNavigation}>
-          {this._renderCancelButton()}
+          {this._renderBackButton()}
         </View>
-        <View style={styles.form}>
-          <View style={styles.inputContainer}>
+        <View style={styles.container}>
+          <View style={styles.signUpForm}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.inputField}
+                  onChangeText={(textUsername) => this.setState({textUsername})}
+                  value={this.state.textUsername}
+                  placeholder={"Username"}
+                  autoFocus={true}
+                  autoCapitalize={'none'}
+                  autoCorrect={false}
+                  keyboardType={'default'}
+                  enablesReturnKeyAutomatically={true}
+                />
+            </View>
+            <View style={styles.inputContainer}>
               <TextInput
                 style={styles.inputField}
-                onChangeText={(textUsername) => this.setState({textUsername})}
-                value={this.state.textUsername}
-                placeholder={"Username"}
-                autoFocus={true}
+                onChangeText={(textEmail) => this.setState({textEmail})}
+                value={this.state.textEmail}
+                placeholder={"E-mail"}
                 autoCapitalize={'none'}
                 autoCorrect={false}
-                keyboardType={'default'}
-            />
+                keyboardType={'email-address'}
+                enablesReturnKeyAutomatically={true}
+                />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.inputField}
+                onChangeText={(textPassword) => this.setState({textPassword})}
+                value={this.state.textPassword}
+                placeholder={"Password"}
+                autoCapitalize={"none"}
+                autoCorrect={false}
+                secureTextEntry={true}
+                enablesReturnKeyAutomatically={true}
+                />
+            </View>
+            {this._renderLoginButton()}
           </View>
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.inputField}
-              onChangeText={(textEmail) => this.setState({textEmail})}
-              value={this.state.textEmail}
-              placeholder={"E-mail"}
-              autoCapitalize={'none'}
-              autoCorrect={false}
-              keyboardType={'email-address'}
-            />
+          <View style={styles.bottomNavigation}>
+            {this._renderTermsLink()}
+            {this._renderSignupLink()}
           </View>
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.inputField}
-              onChangeText={(textPassword) => this.setState({textPassword})}
-              value={this.state.textPassword}
-              placeholder={"Password"}
-              autoCapitalize={"none"}
-              autoCorrect={false}
-              secureTextEntry={true}
-            />
-          </View>
-          {this._renderLoginButton()}
-        </View>
-        <View style={styles.bottomNavigation}>
-          {this._renderTermsLink()}
-          {this._renderSignupLink()}
         </View>
       </View>
     );
@@ -166,7 +186,7 @@ let styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
 
-  cancelButton: {
+  backButton: {
     height: 16,
     width: 16
   },
@@ -177,8 +197,9 @@ let styles = StyleSheet.create({
     marginTop: 36
   },
 
-  form: {
-    marginTop: -MARGIN*20
+  signUpForm: {
+    flex: 1,
+    justifyContent: 'center'
   },
 
   loginButton: {
@@ -187,7 +208,6 @@ let styles = StyleSheet.create({
     width: 375 - MARGIN*8,
     marginTop: 36,
     backgroundColor: Colors.darker,
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
